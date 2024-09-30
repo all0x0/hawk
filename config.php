0a55bc4df627cb6b7f572494ab0db67c57679a7b
all0x0 <131516268+all0x0@users.noreply.github.com>
2024-09-30 21:54:35 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Sensitive configuration settings
 define('TELEGRAM_BOT_TOKEN', '7330752870:AAEhJrg6dnlPtpjUQoaz0NqiNzD6MOqdSwA');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'https://us-east-2.aws.data.mongodb-api.com/app/data-lrjylys/endpoint/tap;
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'havencoin');
